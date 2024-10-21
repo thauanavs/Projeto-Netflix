5b8838e0b13d581df12e09be67154aac5d27ed32
thauanavs <thauanavasconcelos@gmail.com>
2024-10-21 18:00:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+
+botaoSom.addEventListener("click", ligaSom)
+
+function ligaSom(){
+    video.muted = false
+    
+}
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal(){
+    modal.style.display="block"
+}
+
+function esconderModal(){
+    modal.style.display = "none"
+}
